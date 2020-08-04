91fa098bd2c745f3f499e52190fd67d866bc7669
Amirjon <amirqodirov8383@gmail.com>
2020-08-04 16:47:51 +0300

    Deployed project at heroku

--- src/components/TikTok.vue
+++ src/components/TikTok.vue
@@ -131,7 +131,7 @@
                 }
                 this.$axios({
                     method: 'post',
-                    url: 'http://192.168.1.17:9999/tag',
+                    url: 'http://localhost:9999/tag',
                     headers: {
                         'Content-Type': 'application/json',
                         'Accept': 'application/json',
